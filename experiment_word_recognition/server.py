0261c9d4f84d6658bd011d170c9ee13940d1a5dc
McDermott Speaker Array <mcdermottspeakerarray@dhcp-10-29-166-2.dyn.mit.edu>
2023-10-17 14:30:37 -0400

    use index3 as template

--- experiment_word_recognition/server.py
+++ experiment_word_recognition/server.py
@@ -6,34 +6,37 @@ import json
 async def index(request):
     return web.FileResponse(
         "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/experiment_word_recognition/index3.html"
+        # "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/min_eg.html"
     )
     
 async def echo(websocket):
     async for message in websocket:
         data = json.loads(message)
         print(f"got data from client: {data}")
-        await websocket.send(message)
+        
+        if data.get("action", None) != None:
+            # wordResponse = data['response']
+            print(data["action"])
+        else:
+        
+            await websocket.send(message)
     
 async def start_server():
     app = web.Application()
     app.add_routes([web.get("/", index), 
                     web.static("/jspsych/", "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/jspsych"),
-                    web.static("/root/", "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/experiment_word_recognition"),
+                    web.static("/root/", "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/experiment_word_recognition"),#])
                     web.static("/main/", "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/")])
     server = web.AppRunner(app)
     await server.setup()
-    site = web.TCPSite(server, "mcdermottlab.local", 9090)
+    site = web.TCPSite(server, "mcdermottlab.local", 2090)
     await site.start()
-    
+
     ws_server = websockets.serve(echo, "mcdermottlab.local", 8765)
     asyncio.ensure_future(ws_server)
     
     await asyncio.Future() # run forever
-    
-    
+
+
 if __name__ == "__main__":
     asyncio.run(start_server())
-    
-    
-    
-    
