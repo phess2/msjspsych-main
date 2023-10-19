767ac4630485bac346d4cedbd581522ce1bedf00
McDermott Speaker Array <mcdermottspeakerarray@dhcp-10-29-166-2.dyn.MIT.EDU>
2023-10-18 21:06:47 -0400

    min working jspsych with python speaker array epmt

--- experiment_word_recognition/server.py
+++ experiment_word_recognition/server.py
@@ -2,24 +2,26 @@ from aiohttp import web
 import websockets
 import asyncio
 import json
+import pickle 
+import pandas as pd 
+
+# open trial manifest 
+with open("/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/experiment_pilot_manifest_trial_dict.pkl", 'rb') as f:
+    trial_dict = pickle.load(f)
 
 async def index(request):
     return web.FileResponse(
-        "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/experiment_word_recognition/index3.html"
-        # "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/min_eg.html"
+        "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/experiment_word_recognition/spkrm_attn_expmt.html"
     )
     
 async def echo(websocket):
     async for message in websocket:
         data = json.loads(message)
         print(f"got data from client: {data}")
-        
-        if data.get("action", None) != None:
-            # wordResponse = data['response']
-            print(data["action"])
-        else:
-        
-            await websocket.send(message)
+        if data.get("trial_ix", None) != None:
+            print(trial_dict[data.get("trial_ix")], '\n')
+        elif data.get("data", False):
+            print(data.get("data"))
     
 async def start_server():
     app = web.Application()
@@ -29,7 +31,7 @@ async def start_server():
                     web.static("/main/", "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/")])
     server = web.AppRunner(app)
     await server.setup()
-    site = web.TCPSite(server, "mcdermottlab.local", 2090)
+    site = web.TCPSite(server, "mcdermottlab.local", 9090)
     await site.start()
 
     ws_server = websockets.serve(echo, "mcdermottlab.local", 8765)
