157af66cacacbff0b7fc8f09ff25c44a113f16ee
McDermott Speaker Array <mcdermottspeakerarray@dhcp-10-29-241-1.dyn.mit.edu>
2024-10-01 15:56:01 -0400

    Track running changes

--- experiment_spatial_word_recognition_thresholds/server.py
+++ experiment_spatial_word_recognition_thresholds/server.py
@@ -6,6 +6,7 @@ import pickle
 from scipy.io import wavfile
 import pandas as pd 
 import sys
+import os 
 sys.path.append('/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/SpeakerArray/src')
 from utils import speaker_utils
 sys.path.append('/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/SpeakerArray/scripts/exp_runners')
@@ -15,8 +16,6 @@ import numpy as np
 import sounddevice as sd
 from pathlib import Path 
 
-# TODO: Fix bug where mixtures don't play 
-# Check rn.run_exp to make sure target + mixture made correctly 
 
 """
 Before running, make a new manifest and trial dict using `gen_participant_trials.ipynb`
@@ -26,7 +25,7 @@ Checklist of changes to make to this file to run a new participant:
 1. Update PART_IX to match new manifest you just generated.
 2. ensure experiment paths are correct (are you piloting? Is this the main run?)
 3. Source correct work answer key in `spkrm_attn_expmt.html`:
-    1. change key path:
+    1. change key path:k
     <script src="../root/expmt_keys/participant_<ix>_key.js"></script>
     2. update number for trials to match:
         n_trials = <num_trials> 
@@ -37,12 +36,12 @@ Checklist of changes to make to this file to run a new participant:
 # SET EXPERIMENT PARAMS AND VARS
 ##################################
 DB_SPL = 65
-BLOCK_LEN = 120
+BLOCK_LEN = 80
 
-PART_IX = 1
+PART_IX = 14
 EXP_DIR = Path("speaker_array_manifests")
 PART_NAME = f"participant_{PART_IX:03d}"
-EXP_TYPE = "pilot_thresholds_v00"  # Name of sub directory to save experiment results - should match dir of trial dicts!
+EXP_TYPE = "thresholds_v02"  # Name of sub directory to save experiment results - should match dir of trial dicts!
 
 EXPMT_TRIAL_DICT_NAME = f"{EXP_TYPE}/{PART_NAME}_pilot_trial_dict.pkl"
 
@@ -53,12 +52,16 @@ EXPMT_TRIAL_DICT_PATH = EXPMT_TRIAL_DICT_DIR / EXPMT_TRIAL_DICT_NAME
 print(f"Running trial dict {EXPMT_TRIAL_DICT_PATH}")
 with open(EXPMT_TRIAL_DICT_PATH, 'rb') as f:
     trial_dict = pickle.load(f)
+    
+# print(trial_dict)
 
 # Set output data save path
 output_dir = Path("/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/experiment_spatial_word_recognition_thresholds/data")
 output_dir = output_dir / EXP_TYPE 
 output_dir.mkdir(parents=True, exist_ok=True)
 out_name = output_dir/ f"{PART_NAME}.csv"
+# if out_name.exists():
+#     raise FileExistsError("{out_name} already exists! Did you forget to update the participant number variable, PART_IX?")
     
 ###################
 # Set up speaker IO
@@ -72,8 +75,8 @@ if dev == "16A":
 # set break and block sound paths and levels 
 break_soundsr, break_sound = wavfile.read('/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/SpeakerArray/assets/sounds/three_tone.wav')
 block_start_soundsr, block_start_sound = wavfile.read('/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/SpeakerArray/assets/sounds/beep-01a.wav')
-break_sound = speaker_utils.rms_norm(break_sound, DB_SPL)
-block_start_sound = speaker_utils.rms_norm(block_start_sound, DB_SPL)
+break_sound = speaker_utils.set_dba_level(break_sound, DB_SPL)
+block_start_sound = speaker_utils.set_dba_level(block_start_sound, DB_SPL)
 
 #################
 # Init html paths
@@ -112,10 +115,18 @@ async def echo(websocket):
                              break_soundsr,
                              block_start_sound,
                              block_start_soundsr,
-                             block_length=BLOCK_LEN)
+                             block_length=BLOCK_LEN,
+                             dBA=DB_SPL)
         if data['action'] == 'store_data':
             exp_data = pd.DataFrame(json.loads(data['data']))
             exp_data.to_csv(out_name)
+        ## lock data if done - buggy for now 
+        # if trial_ix == len(trial_dict):
+        #     os.chmod(out_name, 0o444) # sets permissions to read only
+        #     file_stat = os.stat(out_name)
+        #     file_permissions = oct(file_stat.st_mode)[-3:]
+        #     print(f'{out_name} permissions set to {file_permissions}')
+            
     return None
 
 ###############
@@ -130,7 +141,7 @@ async def start_server():
                     web.static("/main/", "/Users/mcdermottspeakerarray/Documents/binaural_cocktail_party/msjspsych-main/")])
     server = web.AppRunner(app)
     await server.setup()
-    site = web.TCPSite(server, "mcdermottlab.local", 9090)
+    site = web.TCPSite(server, "mcdermottlab.local", 9091)
     await site.start()
 
     ws_server = websockets.serve(echo, "mcdermottlab.local", 8765, ping_interval=None)
@@ -138,6 +149,6 @@ async def start_server():
 
     await asyncio.Future() # run forever
     return None 
-    
+
 if __name__ == "__main__":
     asyncio.run(start_server())
